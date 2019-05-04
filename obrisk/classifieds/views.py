8cf5303718b4388dab3f6b3aa0acecd36ac90d0a
Elisha Kingdom <elishakingdom@gmail.com>
2019-05-04 12:45:16 +0800

    Setup sts auth in the views

--- obrisk/classifieds/views.py
+++ obrisk/classifieds/views.py
@@ -21,6 +21,106 @@ from obrisk.classifieds.forms import ClassifiedForm
 
 import json
 import re
+import os
+
+from aliyunsdkcore import client
+from aliyunsdksts.request.v20150401 import AssumeRoleRequest
+
+import oss2
+
+
+# The following code shows the usage of STS, including role-playing to get the temporary user's key and using the temporary user's key to access the OSS.
+
+# STSGetting Started Tutorial See https://yq.aliyun.com/articles/57895
+# STS's official documentation can be found at https://help.aliyun.com/document_detail/28627.html
+
+# Initialize the information such as AccessKeyId, AccessKeySecret, and Endpoint.
+# Get through environment variables, or replace something like "< your AccessKeyId>" with a real AccessKeyId.
+# Note: AccessKeyId and AccessKeySecret are the keys of the sub-users.
+# RoleArn can be viewed in the console under Access Control > Role Management > Administration > Basic Information > Arn.
+#
+# Taking the Hangzhou area as an example, the Endpoint can be:
+#   http://oss-cn-hangzhou.aliyuncs.com
+#   https://oss-cn-hangzhou.aliyuncs.com
+# Access by HTTP and HTTPS respectively.
+
+
+access_key_id = os.getenv('OSS_STS_ID')
+access_key_secret = os.getenv('OSS_STS_KEY')
+bucket_name = os.getenv('OSS_BUCKET')
+Endpoint = os.getenv('OSS_ENDPOINT')
+sts_role_arn = os.getenv('OSS_STS_ARN')
+
+# Confirm that the above parameters are filled in correctly.
+for param in (access_key_id, access_key_secret, bucket_name, endpoint, sts_role_arn):
+    Assert '<' not in param, 'Please set the parameter: ' + param
+
+
+class StsToken(object):
+    Temporary user key returned by """AssumeRole
+    :param str access_key_id: access user id of the temporary user
+    :param str access_key_secret: temporary user's access key secret
+    :param int expiration: expiration time, UNIX time, seconds from UTC zero on January 1, 1970
+    :param str security_token: temporary user token
+    :param str request_id: request ID
+    """
+    def __init__(self):
+        self.access_key_id = ''
+        self.access_key_secret = ''
+        self.expiration = 0
+        self.security_token = ''
+        self.request_id = ''
+
+
+def fetch_sts_token(access_key_id, access_key_secret, role_arn):
+    """Sub User Role Playing to Get the Key of a Temporary User
+    :param access_key_id: access key id of the subuser
+    :param access_key_secret: subuser's access key secret
+    :param role_arn: Arn of the STS role
+    :return StsToken: temporary user key
+    """
+    clt = client.AcsClient(access_key_id, access_key_secret, 'cn-hangzhou')
+    req = AssumeRoleRequest.AssumeRoleRequest()
+
+    req.set_accept_format('json')
+    req.set_RoleArn(role_arn)
+    req.set_RoleSessionName('oss-python-sdk-example')
+
+    body = clt.do_action_with_exception(req)
+
+    j = json.loads(oss2.to_unicode(body))
+
+    token = StsToken()
+
+    token.access_key_id = j['Credentials']['AccessKeyId']
+    token.access_key_secret = j['Credentials']['AccessKeySecret']
+    token.security_token = j['Credentials']['SecurityToken']
+    token.request_id = j['RequestId']
+    token.expiration = oss2.utils.to_unixtime(j['Credentials']['Expiration'], '%Y-%m-%dT%H:%M:%SZ')
+
+    return token
+
+
+# Create a bucket object, all Object related interfaces can be done through the Bucket object
+token = fetch_sts_token(access_key_id, access_key_secret, sts_role_arn)
+auth = oss2.StsAuth(token.access_key_id, token.access_key_secret, token.security_token)
+bucket = oss2.Bucket(auth, endpoint, bucket_name)
+
+
+# Upload a string. The object name is motto.txt, and the content is a famous quote.
+# bucket.put_object('motto.txt', 'Never give up. - Jack Ma')
+
+
+# Download to local file
+# Bucket.get_object_to_file('motto.txt', 'local motto.txt')
+
+
+# Delete the object named motto.txt
+# bucket.delete_object('motto.txt')
+
+
+# Clear local files
+# Os.remove(u'local motto.txt')
 
 class ClassifiedsListView(LoginRequiredMixin, ListView):
     """Basic ListView implementation to call the published classifieds list."""
@@ -85,8 +185,6 @@ class CreateClassifiedView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         messages.success(self.request, self.message)
         return reverse('classifieds:list')
-
-
 
 
 class EditClassifiedView(LoginRequiredMixin, AuthorRequiredMixin, UpdateView):
