e347116c18230a7814d34f71f5bb2a8eeeed411e
Bahati Boniface <bahati@dncrafts.cn>
2019-05-14 21:49:33 +0800

    update classified list and chat

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.production'
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
     try:
         from django.core.management import execute_from_command_line
