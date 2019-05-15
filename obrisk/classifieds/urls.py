b2df9d5f6666157bb35b5cd1dd80fda79bee6bc2
Elisha Kingdom <elishakingdom@gmail.com>
2019-05-15 15:51:39 +0800

    Added the tagging feature

--- obrisk/classifieds/urls.py
+++ obrisk/classifieds/urls.py
@@ -1,18 +1,20 @@
 from django.conf.urls import url
+from django.urls import path
+
 from django.views.generic import TemplateView
 
 
-from obrisk.classifieds.views import (ClassifiedsListView, ExpiredListView, CreateOfficialAdView, 
+from obrisk.classifieds.views import (classified_list, CreateOfficialAdView, 
                                      CreateClassifiedView, EditClassifiedView, 
                                      ReportClassifiedView, DetailClassifiedView)
 app_name = 'classifieds'
 urlpatterns = [
-    url(r'^$', ClassifiedsListView.as_view(), name='list'),
+    url(r'^$', classified_list, name='list'),
     url(r'^write-new-classified/$', CreateClassifiedView.as_view(), name='write_new'),
     url(r'^new-official-classified/$', CreateOfficialAdView.as_view(), name='write_new_official'),
-    url(r'^expired/$', ExpiredListView.as_view(), name='expired'),
+    #url(r'^expired/$', ExpiredListView.as_view(), name='expired'),
+    path('tag/<slug:tag_slug>/', classified_list, name='list_by_tag'),
     url(r'^report/(?P<pk>\d+)/$', ReportClassifiedView.as_view(), name='report_classified'),
     url(r'^edit/(?P<pk>\d+)/$', EditClassifiedView.as_view(), name='edit_classified'),
     url(r'^(?P<slug>[-\w]+)/$', DetailClassifiedView.as_view(), name='classified'),
-
 ]
