dcc7ebdb2b1030ffd2fadaf1a5aae127becb76af
Elisha Kingdom <elishakingdom@gmail.com>
2019-05-31 18:27:35 +0800

    Running collectstatic on the local machine

--- config/settings/local.py
+++ config/settings/local.py
@@ -19,11 +19,6 @@ ALLOWED_HOSTS = [
      '*'
 ]
 
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR('staticfiles'))
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = '/static/'
-
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
@@ -51,10 +46,54 @@ EMAIL_HOST = 'localhost'
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-port
 EMAIL_PORT = 1025
 
-# django-debug-toolbar
+
+# ------------------------------------------------------------------------------
+# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration  # noqa F405
+# https://github.com/aliyun/django-oss-storage
+INSTALLED_APPS += ['django_oss_storage', 'django_extensions']  # noqa F405
+
+
+#For running collectstatic local and reduce noise to the server!
+
+#I serve them in oss bucket when scaling up, don't duplicate static files in every server.
+# ------------------------
+STATICFILES_STORAGE = 'django_oss_storage.backends.OssStaticStorage'
+
+# AliCloud access key ID
+OSS_ACCESS_KEY_ID = env('OSS_STS_ID')
+
+# AliCloud access key secret
+OSS_ACCESS_KEY_SECRET = env('OSS_STS_KEY')
+
+# The name of the bucket to store files in
+OSS_BUCKET_NAME = env('OSS_BUCKET')
+
+# The URL of AliCloud OSS endpoint
+# Refer https://www.alibabacloud.com/help/zh/doc-detail/31837.htm for OSS Region & Endpoint
+OSS_ENDPOINT = env('OSS_ENDPOINT')
+
+OSS_COVERAGE_IF_FILE_EXIST = True
+
+OSS_FILE_SAVE_AS_URL = False
+
+# The expire time to construct signed url for private acl bucket.
+# Can be set by OSS_EXPIRE_TIME as environment variable or as Django settings.
+#The default value is 30 days. I took the values from AWS_EXPIRY in sample project
+OSS_EXPIRE_TIME =  60 * 60 * 24 * 7
+
+# The default location for the static files stored in bucket.
+OSS_STATIC_LOCATION = '/static/'
+
+# The default location for your static files
+STATIC_ROOT =  '/static/'
+
+STATIC_URL =  '/static/'
+
+#django-debug-toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
 INSTALLED_APPS += ['debug_toolbar']  # noqa F405
+
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
@@ -71,11 +110,6 @@ import os
 if os.environ.get('USE_DOCKER') == 'yes':
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS += [ip[:-1] + '1' for ip in ips]
-
-# django-extensions
-# ------------------------------------------------------------------------------
-# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
-INSTALLED_APPS += ['django_extensions']  # noqa F405
 
 # Your stuff...
 
