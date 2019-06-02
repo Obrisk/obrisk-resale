60bd800ff9684d83d5304a780d137792b3e6f1c9
Elisha Kingdom <elishakingdom@gmail.com>
2019-06-02 17:33:14 +0800

    uwsgi configs

--- config/wsgi.py
+++ config/wsgi.py
@@ -28,13 +28,14 @@ sys.path.append(os.path.join(app_path, 'obrisk'))
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
-os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+#os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
+
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
     application = Sentry(application)
 # Apply WSGI middleware here.
