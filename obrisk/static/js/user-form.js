6717a1cc626970a9a7fc17545af2e76cf77fc1c7
Bahati Boniface <bahati@dncrafts.cn>
2019-06-22 14:37:36 +0800

    add preview to user profile

--- obrisk/static/js/user-form.js
+++ obrisk/static/js/user-form.js
@@ -132,7 +132,7 @@ const geo_data = [{
 $(document).ready(function () {
 	//Hide the verification code form
 	$("#code").hide();
-	
+
 	//Set the Province and city name defaults.
 	function preselect() {
 		if (!$("input[name='city']").val() && !$("input[name='province_region']").val()) {
@@ -162,141 +162,138 @@ $(document).ready(function () {
 
 // adding a crsf tokken
 function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
+	var cookieValue = null;
+	if (document.cookie && document.cookie !== '') {
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i++) {
+			var cookie = jQuery.trim(cookies[i]);
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) === (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
 }
 var csrftoken = getCookie('csrftoken');
 
 function csrfSafeMethod(method) {
-  // these HTTP methods do not require CSRF protection
-  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	// these HTTP methods do not require CSRF protection
+	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
+	beforeSend: function (xhr, settings) {
+		if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+			xhr.setRequestHeader("X-CSRFToken", csrftoken);
+		}
+	}
 });
 
 
 var verify_counter = 0;
 var code_counter = 0;
-	
-$(function () {	
-    $("#send-code").click(function (event) {
-		
-		if( (isNaN($("#id_phone_number").val())) || ($("#id_phone_number").val().length != 11) 
-		|| ($("#id_phone_number").val().charAt(0) != 1) )
-		{
+
+$(function () {
+	$("#send-code").click(function (event) {
+
+		if ((isNaN($("#id_phone_number").val())) || ($("#id_phone_number").val().length != 11) ||
+			($("#id_phone_number").val().charAt(0) != 1)) {
 			event.preventDefault();
 			bootbox.alert("The phone number you entered is not correct. Don't include the country code!");
-		}
-		else {
+		} else {
 			//If button is disabled and the verification code is not sent, user can't do anything.
 
 			$.ajax({
 				url: '/users/verification-code/',
-				data: { phone_no : $('#id_phone_number').val() } ,
+				data: {
+					phone_no: $('#id_phone_number').val()
+				},
 				cache: false,
 				type: 'GET',
 				success: function (data) {
-					if (data.success == false) 
-					{
+					if (data.success == false) {
 						$("#code-notice").empty().append("<p> User with this number exists or number is incorrect! <p>");
 						//$("#send-code").attr("disabled", false);
-					}else {
+					} else {
 						$("#send-code").attr("disabled", true);
 						$("#code").show();
-						$("#code-notice").empty().append("<p> We have send the verification code, it is valid for 1 minute! <p>");	
+						$("#code-notice").empty().append("<p> We have send the verification code, it is valid for 1 minute! <p>");
 
-						var timeout = 60;						
-					
+						var timeout = 60;
+
 						function updateSec() {
 							timeout--;
-							if (timeout>0) {
-								$("#send-code").text(timeout+" S")
+							if (timeout > 0) {
+								$("#send-code").text(timeout + " S")
 							} else {
 								$("#send-code").text("Get Code")
 								$("#send-code").attr("disabled", false);
 							}
-					}
+						}
 						// repeat with the interval of 1 seconds
 						let timerId = setInterval(() => updateSec(), 1000);
 
 						// after 60 seconds stop
-						setTimeout(() => { clearInterval(timerId); }, 61000);
+						setTimeout(() => {
+							clearInterval(timerId);
+						}, 61000);
 
-					
-						
-						
-						verify_counter = verify_counter + 1;	
-						
-						if(verify_counter >= 5){
+
+
+
+						verify_counter = verify_counter + 1;
+
+						if (verify_counter >= 5) {
 							$("#send-code").attr("disabled", true);
 							bootbox.alert("Maximum number of sending code trials has reached, we can't send anymore!");
 
 						}
-					}			
+					}
 				},
-				error: function(err){
+				error: function (err) {
 					console.log(err);
 				}
 			});
 			return false;
 		}
-		
+
 	});
-	
-	$("#phone-verify").click(function () {   
-		
-		if( (isNaN($("input[name='code']").val())) || ($("input[name='code']").val().length != 6)  || 
-		(isNaN($("#id_phone_number").val())) || ($("#id_phone_number").val().length != 11) 
-		|| ($("#id_phone_number").val().charAt(0) != 1) )
-		{
+
+	$("#phone-verify").click(function () {
+
+		if ((isNaN($("input[name='code']").val())) || ($("input[name='code']").val().length != 6) ||
+			(isNaN($("#id_phone_number").val())) || ($("#id_phone_number").val().length != 11) ||
+			($("#id_phone_number").val().charAt(0) != 1)) {
 			event.preventDefault();
 			bootbox.alert("The code you entered is wrong!");
-		}
-		else {
+		} else {
 			$.ajax({
 				url: '/users/phone-verify/',
-				data: { 
-					phone_no : $('#id_phone_number').val(), 
-					code : $("input[name='code']").val()
+				data: {
+					phone_no: $('#id_phone_number').val(),
+					code: $("input[name='code']").val()
 				},
 				cache: false,
 				type: 'GET',
 				success: function (data) {
 					//enable send button after message is sent
-					if (data.success == false) 
-					{
+					if (data.success == false) {
 						$("#results").empty().append("<p>The verification code is wrong! Please cross-check </p>");
 						$("#send-code").attr("disabled", false);
-						code_counter = code_counter + 1;	
+						code_counter = code_counter + 1;
 
-						if(code_counter >= 5){
+						if (code_counter >= 5) {
 							$("#phone-verify").attr("disabled", true);
 							bootbox.alert("Maximum number of code retrial has reached, you can't retry anymore!");
 						}
-					}
-					else {
+					} else {
 						// $('#send-code').removeAttr("disabled");
 						$("#results").empty().append("<p> You have successfully verified your phone number! <p>");
 						$("input[name='phone_number']").val("+86" + $("input[name='phone_number']").val());
-						
+
 						$("#code-notice").empty()
-						
+
 						$("#code").hide();
 						$("#send-code").hide();
 						$("#id_phone_number").attr("disabled", true);
@@ -305,30 +302,28 @@ $(function () {
 						$("#id_password2").attr("disabled", false);
 					}
 				},
-				error: function(error) {
+				error: function (error) {
 					bootbox.alert(error)
 				}
 			});
 			return false;
 		}
 	});
-	
+
 
 	$("#submit").click(function (event) {
 		if (!$("select[name='city']").val() || !$("select[name='province']")) {
 			event.preventDefault();
 			bootbox.alert("Please enter your address!");
 		}
-		if (!$("input[name='username']").val() || !$("input[name='password1']") || !$("input[name='password2']")  ) {
+		if (!$("input[name='username']").val() || !$("input[name='password1']") || !$("input[name='password2']")) {
 			event.preventDefault();
 			bootbox.alert("Please fill in all of the info. Fill in your phone number first!");
-		} 
-		if ($("input[name='password1']").val() != $("input[name='password2']").val() ) {
+		}
+		if ($("input[name='password1']").val() != $("input[name='password2']").val()) {
 			event.preventDefault();
 			bootbox.alert("Your password's inputs don't match!");
-		}
-		
-		else {
+		} else {
 			$("#id_phone_number").attr("disabled", false);
 			$("input[name='city']").val($("select[name='city']").val());
 			$("input[name='province_region']").val($("select[name='province']").val());
@@ -343,7 +338,27 @@ $(function () {
 	});
 });
 
+/**
+ * Allow users preview upload profile picture
+ */
 
+function uploadPreview(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$('#avatar')
+				.attr('src', e.target.result);
+		};
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+
+/**
+ * reponsible for creating a toast message when user update thier profiles
+ */
 "use strict";
 var iqwerty = iqwerty || {};
 iqwerty.toast = (function () {
@@ -478,16 +493,3 @@ iqwerty.toast = (function () {
 		}
 	};
 })();
-
-function uploadPreview(input) {
-	if (input.files && input.files[0]) {
-		var reader = new FileReader();
-
-		reader.onload = function (e) {
-			$('#avatar')
-				.attr('src', e.target.result);
-		};
-
-		reader.readAsDataURL(input.files[0]);
-	}
-}
