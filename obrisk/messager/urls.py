da5c266d215a4213fd7eb9cfb8615d0b35b9f455
Elisha Kingdom <elishakingdom@gmail.com>
2019-08-11 19:36:27 +0800

    Minor fix classifieds details, home

--- obrisk/messager/urls.py
+++ obrisk/messager/urls.py
@@ -6,8 +6,7 @@ app_name = 'messager'
 urlpatterns = [
     url(r'^$', views.ContactsListView.as_view(), name='contacts_list'),
     url(r'^send-message/$', views.send_message, name='send_message'),
-    url(r'^receive-message/$',
-        views.receive_message, name='receive_message'),
+    url(r'^receive-message/$', views.receive_message, name='receive_message'),
     url(r'^(?P<username>[\w.@+-]+)/$', views.ConversationListView.as_view(),
         name='conversation_detail'),
 ]
