8fc346cfc6020dcafa2ec6305b7530292cd2e78c
Elisha Kingdom <elishakingdom@gmail.com>
2019-08-19 13:36:27 +0800

    Fixed some css on classified, login/signup, user-details

--- obrisk/static/js/listing.js
+++ obrisk/static/js/listing.js
@@ -3,8 +3,9 @@
   var sidebarOverlay = document.getElementsByClassName('sidebar-overlay')[0];
   var container = document.getElementsByClassName('container')[0];
   var sidebarBtnOpen = document.getElementById('sidebarBtnOpen');
+  var sidebarBtnClose = document.getElementById('sidebarBtnClose');
 
-  var openSidebar = function () {
+var openSidebar = function () {
     sidebarOverlay.style.left = '0';
     sidebar.style.left = '0';
     sidebarOverlay.style.top = '0';
@@ -12,14 +13,14 @@
     $(".classifieds").toggleClass("open-sidebar");
   }
 
-  var closeSidebar = function (e) {
+var closeSidebar = function (e) {
     e.cancelBubble = true;
     sidebarOverlay.style.left = '-100%';
     sidebar.style.left = '-100%';
     $(".classifieds").toggleClass("open-sidebar");
-
-  }
+}
 
   sidebarOverlay.addEventListener('click', closeSidebar);
   sidebarBtnOpen.addEventListener('click', openSidebar);
+  sidebarBtnClose.addEventListener('click', closeSidebar);
 })()
