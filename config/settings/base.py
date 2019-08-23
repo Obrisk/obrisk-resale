6a8d08fd14898ea3b90e51996801e27ab9722919
Bahati Boniface <bahati@dncrafts.cn>
2019-08-23 10:24:56 +0800

    update splashscreen url

--- config/settings/base.py
+++ config/settings/base.py
@@ -365,34 +365,47 @@ PWA_APP_ICONS = [    {
  
 
 PWA_APP_SPLASH_SCREEN = [
-        {
-            'src':'/static/img/splashscreens/apple-launch-1125x2436.png',
-            'media':'(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)' 
-        },
-        {
-            'src':'/static/img/splashscreens/apple-launch-750x1334.png',
-            'media':'(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)' 
-        },
-        {
-            'src':'/static/img/splashscreens/apple-launch-1242x2208.png',
-            'media':'(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)' 
-        },
-        {
-            'src':'/static/img/splashscreens/apple-launch-640x1136.png',
-            'media':'(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)' 
-        },
-        {
-            'src':'/static/img/splashscreens/apple-launch-1536x2048.png',
-            'media':'(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)'
-        },
-        {
-            'src':'/static/img/splashscreens/apple-launch-1668x2224.png',
-            'media':'(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)'
-        } ,
-        {
-            'src':'/static/img/splashscreens/apple-launch-2048x2732.png',
-            'media':'(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)'
-        } 
+
+    {
+        'src':"/static/img/splashscreens/iphone5_splash.png",
+        'media':"(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+    },
+    {
+        'src':"/static/img/splashscreens/iphone6_splash.png",
+        'media':"(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+    },
+    {
+        'src':"/static/img/splashscreens/iphoneplus_splash.png",
+        'media':"(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+    },
+    {
+        'src':"/static/img/splashscreens/iphonex_splash.png",
+        'media':"(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+    },
+    {
+        'src':"/static/img/splashscreens/iphonexr_splash.png",
+        'media':"(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+    },
+    {
+        'src':"/static/img/splashscreens/iphonexsmax_splash.png",
+        'media':"(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+    },
+    {
+        'src':"/static/img/splashscreens/ipad_splash.png",
+        'media':"(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+    },
+    {
+        'src':"/static/img/splashscreens/ipadpro1_splash.png",
+        'media':"(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+    },
+    {
+        'src':"/static/img/splashscreens/ipadpro3_splash.png",
+        'media':"(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+    },
+    {
+        'src':"/static/img/splashscreens/ipadpro2_splash.png",
+        'media':"(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+    } 
 ]
 PWA_APP_DIR = '/'
 PWA_APP_LANG = 'en-US'
