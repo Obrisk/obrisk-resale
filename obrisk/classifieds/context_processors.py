57cac368bf1e3ef24e35e301a491ba05212fb801
Elisha Kingdom <elishakingdom@gmail.com>
2019-09-23 18:39:32 +0800

    Improve the check new messages context for anonymour users

--- obrisk/classifieds/context_processors.py
+++ obrisk/classifieds/context_processors.py
@@ -9,25 +9,26 @@ def cached_queries(request):
     if popular_tags == None:
         popular_tags = Classified.objects.get_counted_tags()
     
-    convs = Conversation.objects.get_conversations(request.user).annotate(
-                    unread = Subquery (
-                        Message.objects.filter(
-                            conversation=OuterRef('pk'),
-                        ).values('unread')[:1]
-                    ),
-                    recipient = Subquery (
-                    Message.objects.filter(
-                        conversation=OuterRef('pk'),
-                    ).values('recipient')[:1]
-                )
-            )
-
     new_msgs = None
 
-    if convs.exists():
-        for con in convs:
-            if request.user.id == con.recipient and con.unread:
-                new_msgs = True
-                break
+    if request.user.is_authenticated:
+        convs = Conversation.objects.get_conversations(request.user).annotate(
+                        unread = Subquery (
+                            Message.objects.filter(
+                                conversation=OuterRef('pk'),
+                            ).values('unread')[:1]
+                        ),
+                        recipient = Subquery (
+                        Message.objects.filter(
+                            conversation=OuterRef('pk'),
+                        ).values('recipient')[:1]
+                    )
+                )
+
+        if convs.exists():
+            for con in convs:
+                if request.user.id == con.recipient and con.unread:
+                    new_msgs = True
+                    break
 
     return {'popular_tags': popular_tags, 'new_msgs': new_msgs}
