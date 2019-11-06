7c7298002d45247bfbf48966b569e22ed516c6c4
Elisha Kingdom <elishakingdom@gmail.com>
2019-11-06 14:10:05 +0800

    Login landing page update to stories

--- config/settings/base.py
+++ config/settings/base.py
@@ -133,7 +133,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = 'classifieds:list'
+LOGIN_REDIRECT_URL = 'stories:list'
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = 'account_login'
 
