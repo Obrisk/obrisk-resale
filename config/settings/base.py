8088d9b4f513bff9c60e234c4764f8df3a5d93f7
Elisha Kingdom <elishakingdom@gmail.com>
2019-11-07 16:25:32 +0800

    Added frontend to staticfiles directories in base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -186,7 +186,7 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
-    #('frontend/assets', str(ROOT_DIR.path('frontend/assets'))),   #Gulp does this for us.
+    ('frontend/assets', str(ROOT_DIR.path('frontend/assets'))),
 ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
