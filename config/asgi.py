607ec09c34d1bd19f1bcc2ce923224d112784f5c
Elisha Kingdom <elishakingdom@gmail.com>
2019-11-16 06:06:29 +0800

    added app_path in the asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -3,9 +3,15 @@ ASGI entrypoint. Configures Django and then runs the application
 defined in the ASGI_APPLICATION setting.
 """
 
-import os
+import os, sys
 import django
 from channels.routing import get_default_application
+
+# This allows easy placement of apps within the interior
+# obrisk directory.
+app_path = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), os.pardir))
+sys.path.append(os.path.join(app_path, 'obrisk'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 django.setup()
