c9be409225e0b79129d1466d72fe39cb59973702
Elisha Kingdom <elishakingdom@gmail.com>
2019-11-29 01:24:18 +0800

    updated images batch update to update the few remaining

--- obrisk/utils/images_upload.py
+++ obrisk/utils/images_upload.py
@@ -264,32 +264,32 @@ def bulk_update_classifieds_mid_images(request):
     imgs = ClassifiedImages.objects.all()
 
     for index, img in enumerate(imgs):
+        if img.image_mid_size == '1':
+            d = str(datetime.datetime.now())
+            
+            img_mid_name = "classifieds/" + slugify(str(img.classified.user)) + "/" + \
+                    slugify(str(img.classified.title), allow_unicode=True, to_lower=True) + "/mid-size/" + d + str(index)
+            
+            style_mid = 'image/resize,m_fill,h_400'
 
-        d = str(datetime.datetime.now())
-        
-        img_mid_name = "classifieds/" + slugify(str(img.classified.user)) + "/" + \
-                slugify(str(img.classified.title), allow_unicode=True, to_lower=True) + "/mid-size/" + d + str(index)
-        
-        style_mid = 'image/resize,m_fill,h_400'
+            try:
+                process = "{0}|sys/saveas,o_{1},b_{2}".format(style_mid,
+                                                            oss2.compat.to_string(base64.urlsafe_b64encode(
+                                                                oss2.compat.to_bytes(img_mid_name))),
+                                                            oss2.compat.to_string(base64.urlsafe_b64encode(oss2.compat.to_bytes(bucket_name))))
+                bucket.process_object(img.image, process)
 
-        try:
-            process = "{0}|sys/saveas,o_{1},b_{2}".format(style_mid,
-                                                        oss2.compat.to_string(base64.urlsafe_b64encode(
-                                                            oss2.compat.to_bytes(img_mid_name))),
-                                                        oss2.compat.to_string(base64.urlsafe_b64encode(oss2.compat.to_bytes(bucket_name))))
-            bucket.process_object(img.image, process)
+            
+            except oss2.exceptions.NoSuchKey as e:
+                print('status={0}, request_id={1}'.format(e.status, e.request_id ))
 
-        
-        except oss2.exceptions.NoSuchKey as e:
-            print('status={0}, request_id={1}'.format(e.status, e.request_id ))
-
-        except Exception as e:
-            print (e)
-            messages.error(request, "This is trouble, restart the process!")
-            return HttpResponse("Error in updating mid-size-classifieds images!", content_type='text/plain')
-        else:
-            img.image_mid_size = img_mid_name 
-            img.save()
+            except Exception as e:
+                print (e)
+                messages.error(request, "This is trouble, restart the process!")
+                return HttpResponse("Error in updating mid-size-classifieds images!", content_type='text/plain')
+            else:
+                img.image_mid_size = img_mid_name 
+                img.save()
 
     return redirect('classifieds:list')
 
