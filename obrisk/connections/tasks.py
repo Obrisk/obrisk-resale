03fb3afd0fc36930e4a26b0d64886c2a6432f3c1
Elisha Kingdom <elishakingdom@gmail.com>
2019-12-08 19:22:15 +0800

    successfully configured celery and defined one background task

---
+++ obrisk/connections/tasks.py
@@ -0,0 +1,30 @@
+from celery import shared_task
+from config.celery import app
+from obrisk.users.models import User
+from django.conf import settings
+from django.core.cache import cache
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+
+
+@app.task
+def build_recommended_users():
+    '''This is a background task that checks all current authenticated users and
+    build a connections recommendation list for them. In the near future we have
+    to handle the case of the users that were authenticated only before or after
+    the task and they are excluded from this list'''
+    
+    
+    users = User.objects.all()
+
+    for user in users:
+        if user.is_authenticated():
+            #Will improve this to consider connections of connections.
+            profile_complete_users = users.filter(city=user.city).exclude(thumbnail=None)
+            
+            if profile_complete_users.count > 20:
+                TAGS_TIMEOUT = getattr(settings, 'TAGS_CACHE_TIMEOUT', DEFAULT_TIMEOUT)
+                cache.set('recommended_friends', users, timeout=TAGS_TIMEOUT)
+            else:
+                TAGS_TIMEOUT = getattr(settings, 'TAGS_CACHE_TIMEOUT', DEFAULT_TIMEOUT)
+                cache.set('recommended_friends', users, timeout=TAGS_TIMEOUT)
+        
