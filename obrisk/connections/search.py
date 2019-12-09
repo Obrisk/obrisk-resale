8f8bbbedb5b2717ae82398a9e65c03469d5a2ae6
Elisha Kingdom <elishakingdom@gmail.com>
2019-12-09 23:57:43 +0800

    Fixed major friendship bug and added simple recommendation list

---
+++ obrisk/connections/search.py
@@ -0,0 +1,49 @@
+   
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.http import JsonResponse
+from django.views.generic import ListView
+from taggit.models import Tag
+from obrisk.users.models import User
+from obrisk.utils.helpers import ajax_required
+
+
+class SearchListView(LoginRequiredMixin, ListView):
+    """CBV to contain all the search results"""
+    model = User
+    template_name = "connections/search_results.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        query = self.request.GET.get("query")
+        context["users_list"] = User.objects.filter(username__icontains=query).distinct()
+        context["users_count"] = context["users_list"].count()
+        return context
+
+
+
+# For autocomplete suggestions
+@login_required
+@ajax_required
+def get_suggestions(request):
+    # Convert classifieds objects into list to be
+    # represented as a single list.
+    query = request.GET.get('term', '')
+    users = list(User.objects.filter(title__icontains=query))
+
+
+    # Add all the retrieved classifieds to data_retrieved
+    # list.
+    data_retrieved = users
+    results = []
+    for data in data_retrieved:
+        data_json = {}
+        if isinstance(data, Classified):
+            data_json['id'] = data.id
+            data_json['label'] = data.username
+            data_json['value'] = data.city
+        results.append(data_json)
+    return JsonResponse(results, safe=False)
+
+
