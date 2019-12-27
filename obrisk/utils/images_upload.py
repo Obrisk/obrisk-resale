1b1f762b051938eef813759561785db287b064de
Elisha Kingdom <elishakingdom@gmail.com>
2019-12-27 21:35:03 +0800

    updated the return of images_upload to be a list

--- obrisk/utils/images_upload.py
+++ obrisk/utils/images_upload.py
@@ -154,8 +154,9 @@ def get_oss_auth(request):
 
 def multipleImagesPersist(request, images_list, app, obj):
     ''' This function takes the request, images list, app name (string) and app object
-    and it validates the images list and saves the images to the database.
-    It returns True if the images where saved to the db and False if there is a validation problem '''
+    and it validates the images list and stores them to the database. 
+    It returns images objects list if the images where saved to the db
+    False if there is a validation problem '''
 
     #The code from here onwards assume the first element of images list is undefined.
     #This variable will be used in the end of this code.
@@ -170,6 +171,7 @@ def multipleImagesPersist(request, images_list, app, obj):
     #This is just to help to increase the app post on the website. The user shouldn't be discourage with errors
     #Also most of errors are caused by our frontend OSS when uploading the images so don't return invalid form to user.
     img_mid_name = None
+    saved_objs = []
 
     for index, str_result in enumerate(images_list):
         if str_result.startswith(f'{app}/{request.user.username}') == False:
@@ -230,6 +232,7 @@ def multipleImagesPersist(request, images_list, app, obj):
             if img_mid_name:
                 img_obj.image_mid_size = str_result
             img_obj.save()
+            saved_objs.append(img_obj)
             continue   
 
         except Exception:
@@ -246,6 +249,7 @@ def multipleImagesPersist(request, images_list, app, obj):
             if img_mid_name:
                 img_obj.image_mid_size = str_result
             img_obj.save()
+            saved_objs.append(img_obj)
             continue   
 
         else:
@@ -253,9 +257,9 @@ def multipleImagesPersist(request, images_list, app, obj):
             if img_mid_name:
                 img_obj.image_mid_size = img_mid_name
             img_obj.save()
+            saved_objs.append(img_obj)
 
-    return True
-
+    return saved_objs 
 
 
 @login_required
