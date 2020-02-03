7ed6e42a1280a84790c8126768206235f3a7ea42
ibrahimsamson <toddler1998@yahoo.com>
2020-02-03 18:29:45 +0800

    test connection following

--- obrisk/connections/tests/views_tests.py
+++ obrisk/connections/tests/views_tests.py
@@ -21,11 +21,29 @@ class ConnectionsViewsTests(TestCase):
     def setUp(self):
         self.User = self.make_user("first_user")
         self.other_user = self.make_user("second_user")
+        self.official_user = self.make_user("official_user")
         self.client = Client()
         self.other_client = Client()
+        self.official_client = Client()
         self.client.login(username="first_user", password="password")
         self.other_client.login(username="second_user", password="password")
+        self.official_client.login(username="official_user", password="password")
+        self.user1 = self.make_user('user1')
+        self.user1_client = Client()
+        self.user1_client.login(username='user', password="password")
 
+        self.user2 = self.make_user('user2')
+        self.user2_client = Client()
+        self.user2_client.login(username='user2', password="password")
+
+        self.user3 = self.make_user('user3')
+        self.user3_client = Client()
+        self.user3_client.login(username='user3', password="password")
+
+        self.user4 = self.make_user('user4')
+        self.user4_client = Client()
+        self.user4_client.login(username='user4', password="password")
+        
     def assertResponse200(self, response):
         self.assertEqual(response.status_code, 200)
 
@@ -122,4 +140,64 @@ class ConnectionsViewsTests(TestCase):
         self.assertEqual(len(Friend.objects.sent_requests(second_user)), 0)
         
          
+    def test_friendship_accept(self):
+        users_list = ['user1', 'user2', 'user3', 'user4']
+        first_user = self.User
+        second_user = self.other_user
+
+        #logged_in_users = [first_user, second_user]
+       #for u in users_list:
+       #    self.u= self.make_user(u)
+       #    usr = 'client_'+u
+       #    self.usr= Client()
+       #    self.usr.login(username=u, password="password")
+    
+
        
+        user1 = self.user1
+        user2 = self.user2
+        user3 = self.user3
+        user4 = self.user4
+
+
+        logg_in_users = [second_user, user1, user2, user3, user4]
+      
+     
+        self.assertFalse(Friend.objects.are_friends(first_user, user1))
+        self.assertFalse(Friend.objects.can_request_send(first_user, user2))
+        self.assertFalse(Friend.objects.can_request_send(first_user, user3))
+        self.assertFalse(Friend.objects.can_request_send(first_user, user4))
+        self.assertFalse(Friend.objects.can_request_send(first_user, second_user))
+        reqst1 = Friend.objects.add_friend(first_user, user1)
+        reqst2 = Friend.objects.add_friend(first_user, user2)
+        reqst3 = Friend.objects.add_friend(first_user, user3)
+        reqst4 = Friend.objects.add_friend(first_user, user4)
+        reqst5 = Friend.objects.add_friend(first_user, second_user)
+        #"checking connection sent& recieved"
+        self.assertEqual(len(Friend.objects.requests(first_user)), 0)
+        self.assertEqual(len(Friend.objects.sent_requests(first_user)), 5)
+        reqst1.accept()
+        reqst2.accept()
+        reqst3.accept()
+        reqst4.accept()
+        reqst5.accept()
+        #for u in logg_in_users:
+        #    self.assertFalse(Friend.objects.are_friends(first_user, u))
+        #    self.assertFalse(Friend.objects.can_request_send(first_user, u))
+        #    reqst1 = Friend.objects.add_friend(first_user, u)
+        #    reqst5.accept()
+
+    
+    def test_follower_add(self):
+        official_user = self.official_user
+        user1 = self.user1
+        user2 = self.user2
+        user3 = self.user3
+        user4 = self.user4
+        Follow.objects.add_follower(official_user, user1)
+        Follow.objects.add_follower(official_user, user2)
+        Follow.objects.add_follower(official_user, user3)
+        Follow.objects.add_follower(official_user, user4)
+        self.assertEqual(len(Follow.objects.following(official_user)), 4)
+
+
