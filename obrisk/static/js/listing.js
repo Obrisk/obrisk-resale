871ae2c026834fe6417c2c67463680eae080258c
Boni <terriblegoss@yahoo.fr>
2020-03-06 00:13:56 +0800

    WIP: add modal to classified list

--- obrisk/static/js/listing.js
+++ obrisk/static/js/listing.js
@@ -1,51 +1,78 @@
 //(function () {
-  //var sidebar = document.getElementById('sidebar');
-  //var sidebarOverlay = document.getElementsByClassName('sidebar-overlay')[0];
-  //var container = document.getElementsByClassName('container')[0];
-  ////var sidebarBtnOpen = document.getElementById('sidebarBtnOpen');
-  //var sidebarBtnClose = document.getElementById('sidebarBtnClose');
+//var sidebar = document.getElementById('sidebar');
+//var sidebarOverlay = document.getElementsByClassName('sidebar-overlay')[0];
+//var container = document.getElementsByClassName('container')[0];
+////var sidebarBtnOpen = document.getElementById('sidebarBtnOpen');
+//var sidebarBtnClose = document.getElementById('sidebarBtnClose');
 
-  //var openSidebar = function () {
-    //sidebarOverlay.style.left = '0';
-    //sidebar.style.left = '0';
-    //sidebarOverlay.style.top = '0';
-    //sidebar.style.display = 'block';
-    //$(".classifieds").toggleClass("open-sidebar");
-  //}
+//var openSidebar = function () {
+//sidebarOverlay.style.left = '0';
+//sidebar.style.left = '0';
+//sidebarOverlay.style.top = '0';
+//sidebar.style.display = 'block';
+//$(".classifieds").toggleClass("open-sidebar");
+//}
 
-  //var closeSidebar = function (e) {
-    //e.cancelBubble = true;
-    //sidebarOverlay.style.left = '-100%';
-    //sidebar.style.left = '-100%';
-    //$(".classifieds").toggleClass("open-sidebar");
-  //}
+//var closeSidebar = function (e) {
+//e.cancelBubble = true;
+//sidebarOverlay.style.left = '-100%';
+//sidebar.style.left = '-100%';
+//$(".classifieds").toggleClass("open-sidebar");
+//}
 
-  //sidebarOverlay.addEventListener('click', closeSidebar);
-  //sidebarBtnOpen.addEventListener('click', openSidebar);
-  //sidebarBtnClose.addEventListener('click', closeSidebar);
+//sidebarOverlay.addEventListener('click', closeSidebar);
+//sidebarBtnOpen.addEventListener('click', openSidebar);
+//sidebarBtnClose.addEventListener('click', closeSidebar);
 //})()
 
-function share(title, text, url) {
-  url = window.location.host + url
-  console.log(url)
-  if (window.navigator.share) {
-    window.navigator.share({
-      title: title,
-      text: text,
-      url: url
-    }).
+/* -------------------------------------------------------------------------- */
+/*                              Share classifieds                             */
+/* -------------------------------------------------------------------------- */
 
-    then(function () {
-      return console.log('Successful share');
-    })["catch"](function (error) {
-      return console.log('Error sharing', error);
-    });
+function share(title, text, url) {
+  url = window.location.host + url;
+  console.log(url);
+  if (window.navigator.share) {
+    window.navigator
+      .share({
+        title: title,
+        text: text,
+        url: url
+      })
+      .then(function() {
+        return console.log("Successful share");
+      })
+      ["catch"](function(error) {
+        return console.log("Error sharing", error);
+      });
   } else {
     window.navigator.clipboard.writeText(url);
     bootbox.alert("copied to clipboard");
-
   }
 }
-jQuery('.share').click(function (e) {
+jQuery(".share").click(function(e) {
   e.preventDefault();
-})
+});
+
+/* -------------------------------------------------------------------------- */
+/*                           Classified create ajax                           */
+/* -------------------------------------------------------------------------- */
+
+$(".close-publish").click(function(e) {
+  e.preventDefault();
+  $("input, textarea").val("");
+  $(".app-overlay").removeClass("is-active");
+  $(".is-new-content").removeClass("is-highlighted");
+  $(".is-new-content").addClass("d-none");
+  $(".close-wrap").addClass("d-none");
+  $(".all-stories ").removeClass("block-scroll");
+});
+
+$("#addNewClassified").click(function(e) {
+  e.preventDefault();
+  $(".app-overlay").addClass("is-active");
+  $(".close-wrap").removeClass("d-none");
+  $(".is-new-content").removeClass("d-none");
+  $(".is-new-content").addClass("is-highlighted");
+  $(".all-stories ").addClass("block-scroll");
+});
