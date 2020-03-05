577ac4bedfecbb59c3d13f0194f832121b64a4ed
Boni <terriblegoss@yahoo.fr>
2020-03-05 22:58:53 +0800

    Add: video upload on stories

--- obrisk/static/js/stories.js
+++ obrisk/static/js/stories.js
@@ -250,7 +250,7 @@ $(function() {
   //
   $("body").on("uploadComplete", function(event) {
     $("#id_images").val(images);
-    $("#id_video").val(storyVideo);
+    $("#id_video").val(videos);
     $("#id_img_error").val(img_error);
     $.ajax({
       url: "/stories/post-stories/",
