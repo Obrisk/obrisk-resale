e822bc893b56191b79b09f0b4c34d480634dab60
elshaddae <el@obrisk.com>
2020-03-08 20:41:11 +0800

    remove csrftkn duplicate data

--- obrisk/static/js/stories.js
+++ obrisk/static/js/stories.js
@@ -1,22 +1,4 @@
 $(function() {
-  function getCookie(name) {
-    // Function to get any cookie available in the session.
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== "") {
-      var cookies = document.cookie.split(";");
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = jQuery.trim(cookies[i]);
-        // Does this cookie string begin with the name we want?
-        if (cookie.substring(0, name.length + 1) === name + "=") {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
-      }
-    }
-    return cookieValue;
-  }
-
-  var csrftoken = getCookie("csrftoken");
 
   function csrfSafeMethod(method) {
     // These HTTP methods do not require CSRF protection
@@ -30,6 +12,8 @@ $(function() {
       }
     }
   });
+
+
   function offset(el) {
     var rect = el.getBoundingClientRect(),
       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
