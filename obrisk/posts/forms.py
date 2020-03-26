a1246d4ff546eeef508747973f8095ea4d34df74
elshaddae <el@obrisk.com>
2020-03-27 01:05:17 +0800

    added quill fields on posts

--- obrisk/posts/forms.py
+++ obrisk/posts/forms.py
@@ -1,21 +1,30 @@
 from django import forms
+from django.contrib.postgres.forms import JSONField
 from markdownx.fields import MarkdownxFormField
 from obrisk.posts.models import Post, Comment
+from obrisk.utils.fields import RichTextFormField
 
 
 class PostForm(forms.ModelForm):
     status = forms.CharField(widget=forms.HiddenInput())
     edited = forms.BooleanField(
         widget=forms.HiddenInput(), required=False, initial=False)
-    image = forms.CharField(required=False, max_length=150, label=('Cover photo URL'))
+    image = forms.CharField(
+            required=False, max_length=150,
+            label=('Cover photo URL'))
     content = MarkdownxFormField()
+    content_html = RichTextFormField(
+            widget=forms.HiddenInput(), required=False)
+    content_json = JSONField(
+            widget=forms.HiddenInput(), required=False)
 
     class Meta:
         model = Post
-        fields = ["title", "content", "image", "tags",
+        fields = ["title", "content", "content_html",
+                "content_json", "image", "tags",
                 "status", "edited", "category"]
         help_texts = {
-            "title": "Make it short but descriptive, the maximum is 80 characters.",
+            "title": "Make it short but descriptive, maximum 80 characters.",
         }
 
 
