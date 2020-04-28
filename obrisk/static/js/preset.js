cde7dfadb5efb80c5848e9b6a39aed24ac98aa72
elshaddae <el@obrisk.com>
2020-04-28 20:52:34 +0800

    fixed the stories list without jQuery at the beginning

--- obrisk/static/js/preset.js
+++ obrisk/static/js/preset.js
@@ -1,22 +1,4 @@
 $(function() {
-  function getCookie(name) {
-    // Function to get any cookie available in the session.
-    var cookieValue = null;
-    if (document.cookie) {
-      var cookies = document.cookie.split(";");
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = jQuery.trim(cookies[i]);
-        // Does this cookie string begin with the name we want?
-        if (cookie.substring(0, name.length + 1) === name + "=") {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
-      }
-    }
-    return cookieValue;
-  }
-
-  var csrftoken = getCookie("csrftoken");
 
   function csrfSafeMethod(method) {
     // These HTTP methods do not require CSRF protection
