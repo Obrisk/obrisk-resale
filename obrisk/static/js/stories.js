669e246f7204d49d7cac044efa36e9b7a0a79113
elshaddae <el@obrisk.com>
2020-05-03 09:22:25 +0800

    changed the design of stories

--- obrisk/static/js/stories.js
+++ obrisk/static/js/stories.js
@@ -1,4 +1,46 @@
 $(function () {
+
+  if ('querySelector' in document && 
+	'addEventListener' in window) {
+
+	var toggleButtons = document.querySelectorAll('.toggle-content');
+	var fullTextWrappers = document.querySelectorAll('.fulltext');
+    var shortText = document.querySelector('#short-content');
+	var fullText;
+	var toggleButtonText;
+
+	[].forEach.call(fullTextWrappers, function(fullTextWrapper) {
+		// hide all full text on load
+		fullTextWrapper.setAttribute('hidden', true);
+        shortText.hidden = false;
+	});
+
+	[].forEach.call(toggleButtons, function(toggleButton) {
+		// show toggle more buttons
+		toggleButton.removeAttribute('hidden');
+
+		// add listener for each button
+		toggleButton.addEventListener('click', function () {
+
+			fullTextWrapper = this.parentElement.querySelector('.fulltext');
+			toggleButtonText = this.querySelector('.text');
+
+			// change attributes and text if full text is shown/hidden
+			if (!fullTextWrapper.hasAttribute('hidden')) {
+				toggleButtonText.innerText = 'More';
+				fullTextWrapper.setAttribute('hidden', true);
+				toggleButton.setAttribute('aria-expanded', false);
+                shortText.hidden = false;
+			} else {
+				toggleButtonText.innerText = 'Less';
+				fullTextWrapper.removeAttribute('hidden');
+				toggleButton.setAttribute('aria-expanded', true);
+                shortText.hidden = true;
+			}
+		});
+    });
+  }
+
   var infinite = new Waypoint.Infinite({
     element: $(".infinite-container")[0],
     onBeforePageLoad: function () {
