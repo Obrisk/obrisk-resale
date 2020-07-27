1a57c12cddf67b45522ec5ff102cf422867a1963
elshaddae <el@obrisk.com>
2020-07-27 19:07:14 +0800

    bursting cdn

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,7 @@ from django.conf import settings
 from sentry_sdk.integrations.django import DjangoIntegration
 
 #This has to be updated manually in cases we want rapid deployment
-STATIC_VERSION = 'ver20051701'
+STATIC_VERSION = 'ver2707200001' #DDMMYY####
 
 # GENERAL
 # ------------------------------------------------------------------------------
