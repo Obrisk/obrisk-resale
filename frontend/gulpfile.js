61e3f76d4a81617acf19daacb9877d85257b2240
elshaddae <el@obrisk.com>
2020-07-28 21:27:28 +0800

    upgraded gulp to version 4

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -10,17 +10,17 @@ var concat = require("gulp-concat");
 var wait = require("gulp-wait");
 var nodepath = "node_modules/";
 var assetspath = "../obrisk/static/frontend/assets/";
-var exec = require("child_process").exec;
+var exec = require("child_process").exec;\
+var uglify = require("gulp-uglify");
+
 
 // Watch files for changes
 gulp.task("watch", function() {
   gulp.watch("scss/**/*", ["compile-scss"]);
-  gulp.watch("sass/**/*", ["compile-sass"]);
   gulp.watch("js/**/*", ["copy-js"]);
-  //gulp.watch('images/**/*', ['copy-images']);
 });
 
-// Erases the dist folder
+// Erases the destination folder
 gulp.task("reset", function() {
   rimraf("bulma/*");
   rimraf("scss/*");
@@ -29,44 +29,30 @@ gulp.task("reset", function() {
   rimraf("images/*");
 });
 
-// Erases the dist folder
+// Erases the destination folder
 gulp.task("clean", function() {
-  rimraf("../obrisk/static/frontend/assets");
+  rimraf("../obrisk/static/css");
+  rimraf("../obrisk/static/js");
 });
 
 // Copy Bulma filed into Bulma development folder
 gulp.task("setupBulma", function() {
   //Get Bulma from node modules
-  gulp.src([nodepath + "bulma/*.sass"]).pipe(gulp.dest("bulma/"));
-  gulp.src([nodepath + "bulma/**/*.sass"]).pipe(gulp.dest("bulma/"));
+  gulp.src([nodepath + "bulma-scss/*.scss"]).pipe(gulp.dest("scss/bulma_partials"));
+  gulp.src([nodepath + "bulma-scss/**/*.scss"]).pipe(gulp.dest("scss/bulma_partials"));
 });
 
-// Copy Bulma extensions Sass into Bulma development folder
-gulp.task("extendBulma", function() {
-  gulp
-    .src([nodepath + "bulma-extensions/bulma-divider/dist/bulma-divider.sass"])
-    .pipe(gulp.dest("bulma/sass/extensions/"));
-  gulp
-    .src([nodepath + "bulma-extensions/bulma-steps/dist/bulma-steps.sass"])
-    .pipe(gulp.dest("bulma/sass/extensions/"));
-});
 
 // Copy assets
 gulp.task("copy", function() {
   //Copy other external css assets
   gulp
-    .src(["../obrisk/static/frontend/assets/css/*.css"])
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/css/"));
-  gulp
     .src([nodepath + "@mdi/font/css/materialdesignicons.min.css"])
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/css/"));
+    .pipe(gulp.dest("../obrisk/static/css/"));
   gulp
     .src([nodepath + "dripicons/webfont/webfont.css"])
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/css/"));
+    .pipe(gulp.dest("../obrisk/static/css/"));
   //Copy other external font assets
-  gulp
-    .src(["../obrisk/static/frontend/assets/fonts/*"])
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/fonts/"));
   gulp
     .src([nodepath + "@mdi/font/fonts/**/*"])
     .pipe(gulp.dest("../obrisk/static/frontend/assets/fonts/"));
@@ -79,48 +65,19 @@ gulp.task("copy", function() {
   gulp
     .src([nodepath + "slick-carousel/slick/ajax-loader.gif"])
     .pipe(gulp.dest("../obrisk/static/frontend/assets/css/"));
-  gulp
-    .src([assetspath + "img/**/*"])
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/img/"));
-  //Particles js
-  gulp
-    .src(["../obrisk/static/frontend/assets/js/particles.min.js"])
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/js/"));
 });
 
-//Theme Sass variables
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: "compressed",
-  includePaths: [nodepath + "bulma/sass"]
-};
 
 //Theme Scss variables
 var scssOptions = {
   errLogToConsole: true,
   outputStyle: "compressed",
-  includePaths: ["./scss/partials"]
+  includePaths: ["./scss/partials", "./scss/bulma_partials"]
 };
 
-// Compile Bulma Sass
+
+// Compile Bulma Scss
 gulp.task("compile-sass", function() {
-  var processors = [
-    mq4HoverShim.postprocessorFor({
-      hoverSelectorPrefix: ".is-true-hover "
-    }),
-    autoprefixer({
-      browsers: [
-        "Chrome >= 45",
-        "Firefox ESR",
-        "Edge >= 12",
-        "Explorer >= 10",
-        "iOS >= 9",
-        "Safari >= 9",
-        "Android >= 4.4",
-        "Opera >= 30"
-      ]
-    }) //,
-    //cssnano(),
   ];
   //Watch me get Sassy
   return gulp
@@ -131,6 +88,7 @@ gulp.task("compile-sass", function() {
     .pipe(sourcemaps.write())
     .pipe(gulp.dest("../obrisk/static/frontend/assets/css/"));
 });
+
 
 // Compile Theme Scss
 gulp.task("compile-scss", function() {
@@ -150,33 +108,19 @@ gulp.task("compile-scss", function() {
         "Opera >= 30"
       ]
     }) //,
-    //cssnano(),
+    cssnano(),
   ];
   //Watch me get Sassy
   return gulp
-    .src("./scss/core.scss")
+    .src(["./scss/core.scss", "./scss/bulma.scss"])
     .pipe(wait(500))
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on("error", sass.logError))
     .pipe(postcss(processors))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/css/"));
+    .pipe(gulp.dest("../obrisk/static/css/"));
 });
 
-
-//Copy Theme js to production site
-gulp.task("copy-js", function() {
-  gulp
-    .src(["js/**/*.js", "!js/**/config.js"])
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/js/"));
-});
-
-//Copy images to production site
-gulp.task("copy-images", function() {
-  gulp
-    .src("images/**/*")
-    .pipe(gulp.dest("../obrisk/static/frontend/assets/images/"));
-});
 
 gulp.task("update-sw", function(cb) {
   exec("workbox injectManifest config.js", function(err, stdout, stderr) {
@@ -186,15 +130,14 @@ gulp.task("update-sw", function(cb) {
   });
 });
 
+
 gulp.task("init", ["setupBulma"]);
+
 
 gulp.task("build", [
   "clean",
-  "copy",
-  "copy-js",
-  "compile-sass",
   "compile-scss",
-  "copy-images",
   //"update-sw"
 ]);
+
 gulp.task("default", ["watch", "update-sw"]);
