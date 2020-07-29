715172621bfe80606b96d035bd02b2f5d8687611
elshaddae <el@obrisk.com>
2020-07-29 15:56:42 +0800

    removed frontend folder from settings static list

--- config/settings/base.py
+++ config/settings/base.py
@@ -185,7 +185,6 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
-    ('frontend/assets', str(ROOT_DIR.path('frontend/assets'))),
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
