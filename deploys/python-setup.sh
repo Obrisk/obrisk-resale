4e07f44eecdf0f5e5c0d991cc4540a11101ed06f
elshaddae <el@obrisk.com>
2020-09-01 16:57:31 +0800

    fixed production errors

--- deploys/python-setup.sh
+++ deploys/python-setup.sh
@@ -7,10 +7,6 @@ cd ./locale
 python /home/ubuntu/obdev2018/manage.py compilemessages
 cd ..
 
-cd ./frontend
-/home/ubuntu/obdev2018/frontend/node_modules/gulp/bin/gulp.js build
-cd ..
-
 #Only migrate and collectstatic on one instance
 EC2_INSTANCE_ID=$(ec2metadata --instance-id)
 
@@ -21,4 +17,5 @@ if [ "$EC2_INSTANCE_ID" = "i-09290a52964419c47" ]; then
     python /home/ubuntu/obdev2018/manage.py collectstatic --noinput
 fi
 
+#Copy static files to the local server to serve PWA files & manifest
 python /home/ubuntu/obdev2018/manage.py collectstatic --noinput --settings=config.settings.static
