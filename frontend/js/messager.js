f277d875d21caadb078ceb569c8c69e50b38ea92
elshaddae <el@obrisk.com>
2020-12-03 20:34:57 +0800

    deleted cookie functions on other files except base.html

--- frontend/js/messager.js
+++ frontend/js/messager.js
@@ -31,39 +31,6 @@ $(function() {
     }, 0);
   }
 
-  const setCookie = function(name, value, days) {
-    let expires;
-
-    if (!name && !value) {
-        return false;
-    } else if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        expires = "; expires=" + date.toGMTString();
-    } else expires = "";
-
-    document.cookie = name + "=" + value + expires + "; path=/";
-    return true;
-  };
-
-  const getCookie = function(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  };
-
-  const deleteCookie = function(name) {
-    if (name) {
-      setCookie(name, "", -1);
-      return true;
-    }
-  };
-
   function loadMessages(chat) {
     $.ajax({
       type: "get",
