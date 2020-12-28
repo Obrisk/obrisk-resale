1097ce2260ec34df54ff52c845072ac021c784be
elshaddae <el@obrisk.com>
2020-12-28 18:43:49 +0800

    improved the middleware of visitorid

--- obrisk/utils/middleware.py
+++ obrisk/utils/middleware.py
@@ -5,7 +5,7 @@ VALID_KEY_CHARS = string.ascii_lowercase + string.digits
 
 def visitorid_middleware(get_response):
     def middleware(request):
-        if not request.session.get('visitor_id'):
+        if request.session.get('visitor_id') is None:
             request.session['visitor_id'] = get_random_string(32, VALID_KEY_CHARS)
         response = get_response(request)
         return response
