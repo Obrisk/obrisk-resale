3d56588ed66c8b61c107d1b111171ea2002eaa77
Elisha Kingdom <el@obrisk.com>
2021-02-04 08:52:05 +0000

    fixed import error on classifieds view

--- obrisk/classifieds/views.py
+++ obrisk/classifieds/views.py
@@ -43,7 +43,7 @@ from obrisk.classifieds.forms import (
         ClassifiedForm, OfficialAdForm,
         ClassifiedEditForm)
 from obrisk.utils.images_upload import multipleImagesPersist
-from obrisk.classifieds.wxpayments import get_jsapi_params
+from obrisk.classifieds.wxpayments import get_jsapi_params, get_sign
 from config.settings.base import env
 try:
     from django.contrib.auth import get_user_model
