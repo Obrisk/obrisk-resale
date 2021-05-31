b878044bc58895e515b107836c328093ccc72aab
elkd <el@obrisk.com>
2021-05-31 17:51:01 +0800

    fixed classified order pay results, items without price

---
+++ frontend/js/pay-order.js
@@ -0,0 +1,110 @@
+function wxpayFail() {
+      document.getElementsByClassName('notification')[0].classList.remove('is-hidden'); 
+      document.getElementById(
+              'notf-msg'
+          ).innerHTML = "Sorry payments feature is currently unavailable, contact the seller directly";
+
+      setTimeout(() => {  
+          window.location.href=slugURL;
+      }, 7000);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  fetch(wxCredURL, {
+      headers: {
+    "X-Requested-With": "XMLHttpRequest"
+      },
+      credentials: 'same-origin',
+      redirect: 'follow'
+    }).then (resp => resp.json())
+      .then (strData => {
+          try {
+              let data = JSON.parse(strData);
+              if (data.success === true) {
+                wx.config({
+                    debug: false, 
+                    appId: data.id,
+                    timestamp: data.timestamp,
+                    nonceStr: data.noncestr,
+                    signature: data.signature,
+                    jsApiList: [
+                       'chooseWXPay'
+                    ] 
+                });
+
+              } else {
+              wxpayFail();
+                  console.log('failure to init wx');
+              }
+          } catch {
+              wxpayFail();
+              console.log('failure to init wx');
+          }
+    });
+
+    wx.ready(function(){
+        if (dataError.length > 1) {
+           wxpayFail();
+           console.log(dataError);
+        }else {
+	      try {
+		    wx.chooseWXPay({
+                timestamp: timestamp, 
+                nonceStr: nonceStr,
+                package: packge, 
+                signType: signType,
+                paySign: paySign,
+                trigger: function (res) {
+                    console.log(JSON.stringify(res));
+                },
+                success: function (res) {
+                  let phone = null;
+                  let addr = null; 
+
+                  if (!getCookie('classified-order-is-offline')) {
+                      phone = getCookie('classified-order-phone');
+                      addr = getCookie('classified-order-address');
+                  }
+
+                  fetch(paySuccessURL, {
+                      method : "POST",
+                      body: JSON.stringify({
+                          addr:addr,
+                          phone: phone,
+                          sg:slug
+                      }),
+                      headers: {
+                         "X-Requested-With": "XMLHttpRequest"
+                      },
+                      credentials: 'same-origin',
+                      redirect: 'follow'
+                    }).then (resp => resp.json())
+                      .then (strData => {
+                      let data = JSON.parse(strData);
+                      if (data.success === true) {
+                          window.location.replace= 'classifieds/orders/wsguatpotlfwccdi/' + data.order_slug;
+                      }
+                  })
+                },
+                fail: function (res) {
+                    wxpayFail();
+                    console.log(JSON.stringify(res));
+                },
+                cancel: function (res) {
+                  window.location.replace=slugURL;
+                }
+		    });
+	      } catch {
+              wxpayFail();
+              console.log('failure to init payments, exception happened');
+	      }
+       }
+   });
+
+    wx.error(function(res){
+        wxpayFail();
+        console.log(res);
+    });
+
+});
