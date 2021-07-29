020aea458926a133935a79908cd119575ae568a2
elkd <el@obrisk.com>
2021-07-29 20:37:26 +0800

    fixed the iphone overlay, removed search box and fixed focus margin

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ from requests_aws4auth import AWS4Auth
 
 
 #This has to be updated manually in cases we want rapid deployment
-STATIC_VERSION = 'ver2907210002' #DDMMYY####
+STATIC_VERSION = 'ver2907210003' #DDMMYY####
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 # ALB health check requests should be allowed, whitelist IP address 
