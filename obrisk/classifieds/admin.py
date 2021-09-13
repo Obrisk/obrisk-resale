1333c482e1db3409f6f392641c58333282b7ee12
elkd <el@obrisk.com>
2021-09-13 22:57:31 +0800

    added filters, search and custom actions to classifieds admin

--- obrisk/classifieds/admin.py
+++ obrisk/classifieds/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from obrisk.classifieds.models import (
         Classified, ClassifiedImages,
         ClassifiedTags, ClassifiedOrder
     )
 
 
-admin.site.register(ClassifiedImages)
+def classified_action(modeladmin, request, queryset):
+    for classified in queryset:
+        classified.status = "E"
+        classified.save()
+
+classified_action.short_description = 'Set Expired'
+
+
 admin.site.register(ClassifiedTags)
 admin.site.register(ClassifiedOrder)
 
 @admin.register(Classified)
 class ClassifiedAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'status')
-    list_filter = ('user', 'status', 'timestamp')
+    list_filter = ('status', 'timestamp', 'city')
+    search_fields = ['title', 'user', 'details', 'address']
+    actions = [classified_action,]
+
+
+@admin.register(ClassifiedImages)
+class ClassifiedAdmin(admin.ModelAdmin):
+    list_display = ('classified', 'wx_classified')
+    list_filter = ('classified',)
